2e1406d1e5a77d56a58763524d6702f468c1a491
Tejasvini Dhoble <124334652+tejasvinidhoble@users.noreply.github.com>
2025-03-05 12:16:53 +0530

    Time: 35 ms (96.09%) | Memory: 53.3 MB (14.75%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,16 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+const f = createHelloWorld();
+console.log(f()); // "Hello World"
+console.log(f({}, null, 42)); // "Hello World"
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
