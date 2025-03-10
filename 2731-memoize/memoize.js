f7dc2796bc4c54740ec649e9f7526f427cb5f364
Tejasvini Dhoble <124334652+tejasvinidhoble@users.noreply.github.com>
2025-03-10 10:53:14 +0530

    Time: 277 ms (63.69%) | Memory: 96.2 MB (8.49%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+      let cache = new Map();
+    return function(...args) {
+         let key = JSON.stringify(args); // Create a unique key for caching
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        let result = fn(...args);
+        cache.set(key, result);
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
